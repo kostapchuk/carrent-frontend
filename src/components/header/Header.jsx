5f5743538f08125412dcc1d58970b5b79405a5eb
DaZZeN_2 <qwezmaksim21@gmail.com>
2022-04-06 10:45:26 +0300

    refactor: navbar development

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
     }
 
     return (
-        <div>
+        <div class="container">
             {!LocalStorage.getUserId() && <Link to="/register">Register | </Link>}
             {!LocalStorage.getUserId() && <Link to="/">Login | </Link>}
             <Logout/>
@@ -34,6 +34,29 @@ const Header = () => {
             {LocalStorage.getUserId() && <button onClick={payTheDebt}>Pay</button>}
             {LocalStorage.getUserId() && <Link to="/documents">Documents</Link>}
             <p style={{color: "red"}}>header ends here TYT</p>
+            <button type="button" class="btn btn-primary">Primary</button>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="/register">Register</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="/">Login</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="/cars">Cars</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+            </nav>
         </div>
     );
 }
