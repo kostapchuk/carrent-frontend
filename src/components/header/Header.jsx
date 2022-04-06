03ff3c2c473187aff41c9e344c2663c77632b114
DaZZeN_2 <qwezmaksim21@gmail.com>
2022-04-06 11:48:22 +0300

    navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -25,18 +25,13 @@ const Header = () => {
 
     return (
         <div class="container">
-            {!LocalStorage.getUserId() && <Link to="/register">Register | </Link>}
-            {!LocalStorage.getUserId() && <Link to="/">Login | </Link>}
             <Logout/>
-            <Link to="/cars">Cars</Link> |{" "}
             {LocalStorage.getUserId() && <Link to="/rides">History</Link>}
             {LocalStorage.getUserId() && <p>Balance: {balance} $</p>}
             {LocalStorage.getUserId() && <button onClick={payTheDebt}>Pay</button>}
             {LocalStorage.getUserId() && <Link to="/documents">Documents</Link>}
-            <p style={{color: "red"}}>header ends here TYT</p>
-            <button type="button" class="btn btn-primary">Primary</button>
+
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">Navbar</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -45,10 +40,10 @@ const Header = () => {
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="/register">Register</a>
+                    {!LocalStorage.getUserId() && <a class="nav-link" aria-current="page" href="/register">Register</a>}
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="/">Login</a>
+                    {!LocalStorage.getUserId() && <a class="nav-link" href="/">Login</a>}
                     </li>
                     <li class="nav-item">
                     <a class="nav-link" href="/cars">Cars</a>
