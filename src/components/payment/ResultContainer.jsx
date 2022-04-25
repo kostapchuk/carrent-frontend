2fd437b368faaa0f4d45e9e5eedc35917759282c
k.ostapchuk <xfaseex1@gmail.com>
2022-04-25 20:41:43 +0300

    refactor: fix balance updating

--- src/components/payment/ResultContainer.jsx
+++ src/components/payment/ResultContainer.jsx
@@ -1,4 +1,15 @@
+import {useEffect} from "react";
+import {useDispatch} from "react-redux";
+import {fetchBalance} from "../../slices/BalanceSlice";
+
 const ResultContainer = ({success, message}) => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchBalance())
+    })
+
     return (
         <div className={`container alert d-flex align-items-center ${success ? "alert-success" : "alert-danger"}`} role="alert">
             <div>
