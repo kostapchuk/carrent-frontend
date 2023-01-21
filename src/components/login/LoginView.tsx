c0626e5e4c4de5d4adc88fbea6e448427b38bbb1
kir-ostapchuk <xfaseex1@gmail.com>
2023-01-21 23:48:04 +0300

    refactor: split and simplify login folder

--- src/components/login/LoginView.tsx
+++ src/components/login/LoginView.tsx
@@ -1,6 +1,6 @@
-import {Link} from 'react-router-dom';
 import React, {FC} from "react";
 import {ILoginUser} from "../../types/types"
+import LoginForm from "./LoginForm";
 
 interface LoginViewProps {
     handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void,
@@ -17,42 +17,10 @@ const LoginView: FC<LoginViewProps> = ({handleSubmit, handleFormChange, formUser
                         <div className="card" style={{borderRadius: "15px"}}>
                             <div className="card-body p-5">
                                 <h2 className="text-uppercase text-center mb-5">Log In</h2>
-                                <form onSubmit={handleSubmit}>
-                                    <div className="form-outline mb-4">
-                                        <input type="email"
-                                               className="form-control form-control-lg"
-                                               onChange={handleFormChange}
-                                               name="email"
-                                               value={formUser.email}
-                                               placeholder="Your Email"
-                                        />
-                                    </div>
-                                    <div className="form-outline mb-4">
-                                        <input type="password"
-                                               className="form-control form-control-lg"
-                                               onChange={handleFormChange}
-                                               name="password"
-                                               value={formUser.password}
-                                               placeholder="Password"
-                                        />
-                                    </div>
-                                    <div className="d-flex justify-content-center">
-                                        <button
-                                            className="btn btn-primary btn-block btn-lg gradient-custom-4 text-body">
-                                            Log In
-                                        </button>
-                                    </div>
-                                    <div className="d-flex justify-content-center pt-4">
-                                        <div className="form-check form-switch">
-                                            <input className="form-check-input" type="checkbox" role="switch"
-                                                   id="flexSwitchCheckDefault"/>
-                                            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Remember
-                                                me</label>
-                                        </div>
-                                    </div>
-                                    <p className="text-center text-muted mt-5 mb-0">Create new account <Link
-                                        to="/register" className="fw-bold text-body"><u>Register here</u></Link></p>
-                                </form>
+                                <LoginForm handleSubmit={handleSubmit}
+                                           handleFormChange={handleFormChange}
+                                           formUser={formUser}
+                                />
                             </div>
                         </div>
                     </div>
